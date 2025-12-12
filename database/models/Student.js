c653c56e8ea081be7a8ad9bbc0cd35abb643a653
Ashlee <ashlee.tolentino8@gmail.com>
2025-12-12 01:02:46 -0500

    edited my models/students.js code

--- database/models/Student.js
+++ database/models/Student.js
@@ -9,13 +9,46 @@ const db = require('../db');  // Import Sequelize database instance called "db"
 const Student = db.define("student", {
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notEmpty: true,        // cannot be empty string
+    },
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
-  }
+    allowNull: false,
+    validate: {
+      notEmpty: true,        // cannot be empty string
+    },
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,         // must be a valid email address
+    },
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    // default student image if none is provided
+    defaultValue:
+      'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg',
+  },
+
+  gpa: {
+    // GPA between 0.0 and 4.0, e.g., 3.5
+    type: Sequelize.DECIMAL(2, 1),
+    allowNull: true,         // GPA can be missing
+    validate: {
+      min: 0.0,
+      max: 4.0,
+    },
+  },
 });
 
 // Export the student model
