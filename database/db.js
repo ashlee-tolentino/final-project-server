a93281f19d257a109d59585a57117bf3ee64c1fc
Ashlee <ashlee.tolentino8@gmail.com>
2025-12-12 00:22:09 -0500

    edited db.js file

--- database/db.js
+++ database/db.js
@@ -20,5 +20,107 @@ const db = new Sequelize(dbName, dbUser, dbPwd, {
   dialect: 'postgres'
 });
 
-// Export Sequelize instance, which will be modified with models.
-module.exports = db;
+
+
+
+/*==================================================
+  MODEL DEFINITIONS
+==================================================*/
+
+// Campus model:
+//  - name: not null / not empty
+//  - address: not null / not empty
+//  - description: large text string, can be null
+//  - imageUrl: default value, can be null
+const Campus = db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,    // “large text string”
+    allowNull: true,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue:
+      'https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg',
+  },
+});
+
+// Student model:
+//  - firstName: not null / not empty
+//  - lastName: not null / not empty
+//  - email: not null / not empty, must be a valid email
+//  - imageUrl: default value, can be null
+//  - gpa: decimal(2,1), 0.0–4.0, can be null
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+    defaultValue:
+      'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg',
+  },
+  gpa: {
+    type: Sequelize.DECIMAL(2, 1),
+    allowNull: true,
+    validate: {
+      min: 0.0,
+      max: 4.0,
+    },
+  },
+});
+
+/*==================================================
+  ASSOCIATIONS
+==================================================*/
+
+// A campus can have many students
+Campus.hasMany(Student);
+
+// A student belongs to at most one campus
+Student.belongsTo(Campus);
+
+/*==================================================
+  EXPORTS
+==================================================*/
+
+// Export the Sequelize instance AND the models
+module.exports = {
+  db,
+  Campus,
+  Student,
+};
