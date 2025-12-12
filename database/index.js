abff212d79ee9c819b6577d3ff303fd6e9398980
Ashlee <ashlee.tolentino8@gmail.com>
2025-12-12 03:40:07 -0500

    fixed some errors

--- database/index.js
+++ database/index.js
@@ -5,6 +5,6 @@ It exports both database instance and models so that "App.js" file can import th
 ==================================================*/
 const db = require('./db');  // Database instance
 
-require('../database/models');  // Export models
+require('./models');  // Export models
 
 module.exports = db;  // Export database instance
