776464df2d538a7e4cf68a307bb8585b2cc48574
Ashlee <ashlee.tolentino8@gmail.com>
2025-12-12 01:13:02 -0500

    edited my seedDB.js file

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -7,40 +7,51 @@ const { Campus, Student } = require('../models');  // Import database models
 
 // Seed database
 const seedDB = async () => {
-	// Create a new campus
-	const dummy_campus = await Campus.create({
-		name: "Hunter College",
-		address: "695 Park Ave, New York, NY 10065",
-		description: "This is a school in New York, New York."
-	});
-	// Create a new campus
-	const dummy_campus2 = await Campus.create({
-		name: "Queens College",
-		address: "65-30 Kissena Blvd, Queens, NY 11367",
-		description: "This is a school in Queens, New York."
-	});
-	// Create a new campus
-	const dummy_campus3 = await Campus.create({
-		name: "Brooklyn College",
-		address: "2900 Bedford Ave, Brooklyn, NY 11210",
-		description: "This is a school in Brooklyn, New York."
-	});
-	
-	// Create a new student for a campus
-	const dummy_student = await Student.create({
-		firstname: "Joe",
-      lastname: "Smith",
-	});
-	// Create a new student for a campus
-	const dummy_student2 = await Student.create({
-		firstname: "Mary",
-      lastname: "Johnson",
-	});
+  // Create campuses
+  const hunter = await Campus.create({
+    name: "Hunter College",
+    address: "695 Park Ave, New York, NY 10065",
+    description: "This is a school in New York, New York.",
+    imageUrl: "https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg"
+  });
 
-	// Add students to campuses
-	await dummy_student.setCampus(dummy_campus);
-	await dummy_student2.setCampus(dummy_campus2);
-}
+  const queens = await Campus.create({
+    name: "Queens College",
+    address: "65-30 Kissena Blvd, Queens, NY 11367",
+    description: "This is a school in Queens, New York.",
+    imageUrl: "https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg"
+  });
+
+  const brooklyn = await Campus.create({
+    name: "Brooklyn College",
+    address: "2900 Bedford Ave, Brooklyn, NY 11210",
+    description: "This is a school in Brooklyn, New York.",
+    imageUrl: "https://images.pexels.com/photos/207691/pexels-photo-207691.jpeg"
+  });
+
+  // Create students (REMEMBER: email is required!)
+  const joe = await Student.create({
+    firstname: "Joe",
+    lastname: "Smith",
+    email: "joe.smith@example.com",
+    gpa: 3.2,
+    imageUrl: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg"
+  });
+
+  const mary = await Student.create({
+    firstname: "Mary",
+    lastname: "Johnson",
+    email: "mary.johnson@example.com",
+    gpa: 3.8,
+    imageUrl: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg"
+  });
+
+  // Assign students to campuses
+  await joe.setCampus(hunter);
+  await mary.setCampus(queens);
+
+  console.log("-------- Successfully seeded database --------");
+};
 
 // Export the database seeding function
 module.exports = seedDB;
